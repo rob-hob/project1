1248727a88d58eb475f490708d2fc09a2e48d961
Ivan <godlevskiji@gmail.com>
2020-06-30 15:50:20 +0700

    prog v1

--- js/js/script.js
+++ js/js/script.js
@@ -1,27 +1,18 @@
 "use strict";
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
-second = 10;
-console.log(second/0);
-console.log('stfg' * 9);  
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const persone = "Alex";
-const obj = {
-    name: "ALex",
-    age: 25,
-    isMarried: false
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(obj['age'] = 11);
-let arr = [24, "rob", '123'];
-console.log(arr[1]);
+let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+    estimation = prompt('На сколько оцените его?', '');
 
-const answers = [];
+personalMovieDB.movies[lastFilm] = estimation; 
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-document.write(answers);
+console.log(personalMovieDB);
