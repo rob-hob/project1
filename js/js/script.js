adc93bcdd47669b6893b8a41f100b58194543f9c
Ivan <godlevskiji@gmail.com>
2020-06-30 14:43:48 +0700

    first commit

---
+++ js/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+second = 10;
+console.log(second/0);
+console.log('stfg' * 9);  
+
+const persone = "Alex";
+const obj = {
+    name: "ALex",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj['age'] = 11);
+let arr = [24, "rob", '123'];
+console.log(arr[1]);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
